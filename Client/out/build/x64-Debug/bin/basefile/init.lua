e1646b97bfc855bd97b2658e5981ad438927fd06
hechuangguo <411763942@qq.com>
2026-06-14 16:27:55 +0800

    优化目录

--- Client/out/build/x64-Debug/bin/basefile/init.lua
+++
@@ -1,2 +0,0 @@
-require("data_table")
-return DataTable
--- Client/out/build/x64-Debug/bin/database/item_config.lua
+++
@@ -1,5 +0,0 @@
--- AUTO-GENERATED placeholder from datadosc/item.xlsx
-return {
-    [1001] = { id = 1001, name = "小还丹", desc = "恢复少量生命" },
-    [1002] = { id = 1002, name = "大还丹", desc = "恢复大量生命" },
-}
